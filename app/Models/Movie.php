d18047df5500f9dbef3692dbb216ab18c05134ad
Pomelo <64297316+krumma147@users.noreply.github.com>
2023-08-05 01:26:38 +0700

    Create basic entity including User, Movie and Cat

---
+++ app/Models/Movie.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory;
+    protected $table = "movie";
+    protected $fillable = [
+        "title",
+        // "genre",
+        "status",
+        "original_title",
+        "overview",
+        "origin_country",
+        "language",
+        "revenue"
+    ];
+}
